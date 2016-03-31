a7241d19003f97571ad26d95d57daa34a6d97d9d
yamaguchi-am <qzy13700yamaguchi@gmail.com>
2016-04-01 00:04:23 +0900

    Initial upload of the Physical Flick keyboard files.

---
+++ mozc-furikku/arduino/flick/flick_keyboard.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#define ROWS 5
+#define COLS 12  // Equals to the number of the keys.
+
+enum Direction {
+  CENTER, LEFT, UP, RIGHT, DOWN, NONE
+};
+
+struct SensorData {
+  uint16_t axes[COLS * 2];
+  bool button[COLS];
+};
+
+class FlickKeyboard {
+ private:
+  Direction lastState[COLS];
+  Direction ConvertToFlickState(int16_t x, int16_t y,
+      bool buttonPressed);
+ public:
+  FlickKeyboard();
+  void ProcessSensorData(const SensorData& data, size_t maxKeyNum,
+      const char** outputStrings, int* nOutputs);
+};
+
