399594ba70644d10ea47bd2baabbb095e28e9e13
RT Corporation <shop@rt-net.jp>
2017-03-31 23:15:24 +0900

    license changed
    fixed

--- third_party/mozc-puchi/handheld/Arduino/handheld/handheld.ino
+++ third_party/mozc-puchi/handheld/Arduino/handheld/handheld.ino
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Google Japan G.K.
+Copyright 2017 Google Japan Inc.
 	
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
